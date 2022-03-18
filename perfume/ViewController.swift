91033b5756504aeeffa152e4946b7779d8b2116e
hodoovely <98301931+hodoovely@users.noreply.github.com>
2022-03-18 13:14:32 +0900

    my first iOS project

--- perfume/ViewController.swift
+++ perfume/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func buttonSelectPressed(_ sender: UIButton) {
+        var perfumeArray = [UIImage(named: "perfume01"), UIImage(named: "perfume02"), UIImage(named: "perfume03"), UIImage(named: "perfume04"), UIImage(named: "perfume05"),  UIImage(named: "perfume06"),  UIImage(named: "perfume07"),  UIImage(named: "perfume08")]
+        imageView.image = perfumeArray[Int.random(in: 0 ... 7)]
     }
-
+    
 
 }
 
